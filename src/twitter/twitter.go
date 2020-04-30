73aa85723dfd79dadc498546b59f42affcd42837
Lars Rehe <lars.rehe@gmail.com>
2020-04-30 14:50:34 -0700

    cleaning up JSON data

--- src/twitter/twitter.go
+++ src/twitter/twitter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"os"
@@ -15,6 +16,30 @@ var (
 	accessSecret   = os.Getenv("ACCESS_SECRET_TWITTER")
 )
 
+type cleanTweet struct {
+	Id       string
+	Text     string
+	Likes    int
+	Retweets int
+	Language string
+	URL      string
+}
+
+var CleanTweets []cleanTweet
+
+func getCleanTweets(tweet anaconda.Tweet) cleanTweet {
+	var t = cleanTweet{tweet.IdStr, tweet.Text,
+		tweet.FavoriteCount, tweet.RetweetCount,
+		tweet.Lang, "www.twitter.com/i/web/status/" + tweet.IdStr}
+	return t
+}
+
+func PrettyPrintTweet(tweet anaconda.Tweet) {
+	t := getCleanTweets(tweet)
+	tweetJSON, _ := json.MarshalIndent(t, "", "\t")
+	fmt.Println(string(tweetJSON))
+}
+
 func main() {
 	api := anaconda.NewTwitterApiWithCredentials(
 		accessToken, accessSecret, consumerKey, consumerSecret)
@@ -27,14 +52,14 @@ func main() {
 		len(searchResult.Statuses))
 
 	for _, tweet := range searchResult.Statuses {
-		if tweet.FavoriteCount > 5000 && tweet.RetweetCount > 2000 {
+		if tweet.FavoriteCount > 2000 && tweet.RetweetCount > 500 {
 			_, err := api.Retweet(tweet.Id, false)
 			if err != nil {
 				fmt.Println("Error in Retweeting")
 				continue
 			}
 		} else {
-			fmt.Printf("Skipping tweet with %v retweets and %v likes\n", tweet.RetweetCount, tweet.FavoriteCount)
+			PrettyPrintTweet(tweet)
 		}
 	}
 }
