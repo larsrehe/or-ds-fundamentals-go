3124aa7099be5a8857453b5a4798903106fb2dd1
Lars Rehe <lars.rehe@gmail.com>
2020-04-30 15:16:17 -0700

    saving and loading tweets to/from file

--- src/twitter/twitter.go
+++ src/twitter/twitter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/url"
 	"os"
 
@@ -40,7 +41,38 @@ func PrettyPrintTweet(tweet anaconda.Tweet) {
 	fmt.Println(string(tweetJSON))
 }
 
+func SaveTweetsJSON(TweetsJSON []cleanTweet) error {
+	tweetJSON, _ := json.MarshalIndent(TweetsJSON, "", "\t")
+	err := ioutil.WriteFile("tweets.json", tweetJSON, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadTweetsJSON() ([]cleanTweet, error) {
+	fileData, err := ioutil.ReadFile("tweets.json")
+
+	if err != nil {
+		return CleanTweets, err
+	}
+	err = json.Unmarshal(fileData, &CleanTweets)
+	if err != nil {
+		return CleanTweets, err
+	}
+	return CleanTweets, nil
+}
+
 func main() {
+	Tweets, err := LoadTweetsJSON()
+	if err == nil {
+		fmt.Println("Loading Tweets First.")
+		for _, t := range Tweets {
+			tweetJSON, _ := json.MarshalIndent(t, "", " \t")
+			fmt.Println(string(tweetJSON))
+		}
+	}
+
 	api := anaconda.NewTwitterApiWithCredentials(
 		accessToken, accessSecret, consumerKey, consumerSecret)
 	fmt.Println("Started the API...")
@@ -50,16 +82,19 @@ func main() {
 
 	fmt.Printf("Retrieved %v tweets\n",
 		len(searchResult.Statuses))
-
+	var TweetsForFile []cleanTweet
 	for _, tweet := range searchResult.Statuses {
-		if tweet.FavoriteCount > 2000 && tweet.RetweetCount > 500 {
-			_, err := api.Retweet(tweet.Id, false)
-			if err != nil {
-				fmt.Println("Error in Retweeting")
-				continue
-			}
+		if !tweet.Retweeted && tweet.FavoriteCount > 20 && tweet.RetweetCount > 50 {
+			TweetsForFile = append(TweetsForFile, getCleanTweets(tweet))
 		} else {
-			PrettyPrintTweet(tweet)
+			fmt.Println("Skipping tweet")
+			// PrettyPrintTweet(tweet)
 		}
+	}
+	err = SaveTweetsJSON(TweetsForFile)
+	if err != nil {
+		fmt.Println("Error in saving tweets")
+	} else {
+		fmt.Println("Successfully saved popular tweets")
 	}
 }
